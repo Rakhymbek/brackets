2debb8440a283f446eb88948f233204794eded35
Rakhymbek Sagyn <sagrakha@gmail.com>
2022-02-24 22:57:09 +0600

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  const pairs = {};
+
+  for(let arr of bracketsConfig) {
+      pairs[arr[1]] = arr[0];
+  }
+
+    for(let i = 0; i < str.length; i++) {
+        let currentBracket = str[i];
+        let topElem = stack[stack.length - 1];
+        
+        if(Object.keys(pairs).includes(currentBracket) && topElem === pairs[currentBracket]) {
+          if(pairs[currentBracket] !== stack.pop()) {
+            return false;
+          }
+        }else {
+          stack.push(currentBracket);
+        }
+        console.log(stack);
+    }
+
+    return stack.length === 0;
 }
